8d7fb7e525d1f41d7b6f86c179528844d1b3d235
Chriztian Manzano <cristianorb@unicauca.edu.co>
2022-11-15 11:22:27 -0500

    create project

---
+++ src/main/java/cristian/asae/project_bd_asae/ProjectBdAsaeApplication.java
@@ -0,0 +1,13 @@
+package cristian.asae.project_bd_asae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBdAsaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBdAsaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cristian/asae/project_bd_asae/ProjectBdAsaeApplicationTests.java
@@ -0,0 +1,13 @@
+package cristian.asae.project_bd_asae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBdAsaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
